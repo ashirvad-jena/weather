63ec3088d917f14883d75131ae8cd97b65333437
Ashirvad Jena <>
2021-05-15 13:58:34 +0530

    Search city ui added and folders organised

--- Weather/SearchCity/ViewController/SearchCityViewController.swift
+++ Weather/SearchCity/ViewController/SearchCityViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class SearchCityViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
